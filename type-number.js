87587280789e3e7769ae6bb3a1927cfbb948971b
Hylluh <everpivot6@gmail.com>
2023-06-01 18:26:52 -0300

    exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3; //Isso e uma variavel
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero/numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not A Number (nao e um numero)
+
+const sophya = "Sophya";
+console.log(sophya * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Ola, mundo!";
+const texto2 = 'Ola, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringNumeros = "34567";
+
+const citacao = 'Meu nome e ';
+const meuNome = "Sophya"
+
+// concatenacao (+)
+
+console.log(citacao + meuNome);
+
+// template string OU template literal
