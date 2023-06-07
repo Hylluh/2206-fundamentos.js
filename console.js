e67a40951d773dd761433276714470dd6aa25f2f
Hylluh <everpivot6@gmail.com>
2023-06-06 22:55:25 -0300

    aula 3, 4 e 5

---
+++ console.js
@@ -0,0 +1,14 @@
+// log -> registro
+
+const minhaVar = true
+
+// console.log(234)
+// console.log("eu sou um texto")
+// console.log(minhaVar)
+
+// tratamento de erro!
+
+// console.error("deu erro!")
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
