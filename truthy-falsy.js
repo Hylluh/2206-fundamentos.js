cb2823069f0d17bfacc318377c41974b242c88ad
Hylluh <everpivot6@gmail.com>
2023-06-06 21:07:28 -0300

    codigos var e conversoes

---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// boolean
+
+// const UsuarioLogado = true
+// const ContaPaga = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false) //falsy
+// console.log("" == false) //falsy
+// console.log(1 == true) //truthy
+
+// null ==> vazio ou nada
+// undefined ==> nao definido
+
+let minhaVar;
+let varNull = null;
+
+// console.log(minhaVar)
+// console.log(varNull)
+
+let numero = 3
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
